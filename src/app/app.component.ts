563c8003efe4e7588b050f6578eaf95313c9edac
Samuelmwaniki <smwaniki549@gmail.com>
2024-01-11 14:58:36 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TaskService } from './tasks.service';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+interface Task {
+  id: number;
+  name: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  providers: [TaskService, HttpClient]
 })
 export class AppComponent {
-  title = 'tasks-ui';
+  tasks: Task[];
+  task: string;
+  taskService: TaskService;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor(
+  ) {
+
+    this.tasks = [];
+    this.task = '';
+
+    this.taskService = inject(TaskService);
+  }
+  title = 'task-ui';
+  ngOnInit() {
+    this.taskService.getTasks().subscribe((any) => {
+      console.log(any);
+      this.tasks = any as Task[];
+    });
+  }
+
+  addTask(task: string) {
+    this.taskService.addTask(task).subscribe();
+    this.task ='';
+  }
+
+  deleteTask(id: number) {
+    this.taskService.deleteTask(id).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
